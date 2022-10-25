7ac90e7c34a304739bc5bc2a91b584f796acb106
Lucrecio Paes <lcpaes40@gmail.com>
2022-10-25 16:17:24 -0300

    Lists Collections

--- src/br/edu/lucrecio/collections/list/ExemploList.java
+++ src/br/edu/lucrecio/collections/list/ExemploList.java
@@ -1,6 +1,8 @@
 package br.edu.lucrecio.collections.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExemploList {
@@ -36,6 +38,45 @@ public class ExemploList {
 /*        System.out.println("Imprima as notas uma a baixo da outra: ");
         for (Double nota: notas) System.out.println(nota);*/
 
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2)); // get
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a menor nota " + Collections.min(notas)); // Collections.min(collection)
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas)); //Collections.max(collection)
+
+        System.out.println("Exiba a soma dos valores"); // iterator()
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("A soma dos valores da lista é: " + soma);
+
+        System.out.println("Exiba a media das notas do aluno " + (soma / notas.size())); // size()
+
+        System.out.println("Remova a nota 0.0"); //remove( objeto ou indice )
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota da posição 0");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()){
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista "); // .clear()
+        notas.clear();
+
+        System.out.println("Confira se a lista esta vazia "); // isEmpty()
+        System.out.println(notas.isEmpty());
 
 
     }
